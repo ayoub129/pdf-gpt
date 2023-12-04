8e1f2e03113f003f8df17bb7d203afb34b98b396
Ayoub Berouijil <80260703+ayoub129@users.noreply.github.com>
2023-12-04 15:19:38 +0100

    Update ChatGPT.php

--- ChatGPT.php
+++ ChatGPT.php
@@ -1,6 +1,5 @@
 <?php
-
-function ReqResGPT($api_key, $input_text)
+function ReqResGPT($api_key, $input_text, $pdfFilePath)
 {
     // Set the OpenAI endpoint for chat completions
     $endpoint = 'https://api.openai.com/v1/chat/completions';
@@ -11,48 +10,144 @@ function ReqResGPT($api_key, $input_text)
         'Authorization: Bearer ' . $api_key,
     ];
 
-    // Set the data for the API request
-    $data = [
-        'model' => 'gpt-3.5-turbo-1106',  // Specify the chat model
-        'messages' => [
-            ['role' => 'system', 'content' => 'You are a helpful assistant.'],
-            ['role' => 'user', 'content' => "Analyze the following text and provide a report number and the sample description of the item , and could you tell me the analytical (tests) Names and their ratings (all of them in the text) ,and the analytical summary from the text and i need the response to be like this: Report Number: (the number)\n\nItem Description:\n(item description)\n\nAnalytical Summary:\n (the summary)\n\nNames and Ratings:\n(the name) - (the rating) \n\n " . $input_text],
-        ],
-        'max_tokens' => 1000,  // You can adjust this based on the desired length of the completion
-    ];
+    if ($pdfFilePath != "") {
+        // Initialize and load PDF Parser library
+        $parser = new \Smalot\PdfParser\Parser();
 
-    // Initialize cURL session
-    $ch = curl_init($endpoint);
+        // Specify the number of pages to process in each chunk
+        $pagesPerChunk = 10;
 
-    // Set cURL options
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        // Parse pdf file using Parser library
+        $pdf = $parser->parseFile($pdfFilePath);
 
-    // Execute cURL session and get the response
-    $response = curl_exec($ch);
-    print_r($response);
-    // Check for cURL errors
-    if (curl_errno($ch)) {
-        echo 'Curl error: ' . curl_error($ch);
-        // Handle the error as needed
-    }
+        // Get the total number of pages in the PDF
+        $totalPages = count($pdf->getPages());
 
-    // Close cURL session
-    curl_close($ch);
+        // Calculate the number of chunks
+        $numChunks = ceil($totalPages / $pagesPerChunk);
 
-    // Decode the JSON response
-    $response_data = json_decode($response, true);
+        // Initialize an array to store the response from each chunk
+        $responses = [];
 
-    // Check if 'choices' key exists and is not null
-    if (isset($response_data['choices']) && !empty($response_data['choices'])) {
-        $choices = $response_data['choices'];
-        $generated_text = $choices[0]['message']['content'];
-        // Extracted content from the user message
-        return $generated_text;
+        // Iterate through each chunk
+        for ($chunkNumber = 0; $chunkNumber < $numChunks; $chunkNumber++) {
+            // Calculate the starting page for the current chunk
+            $startPage = $chunkNumber * $pagesPerChunk + 1;
+
+            // Calculate the ending page for the current chunk
+            $endPage = min(($chunkNumber + 1) * $pagesPerChunk, $totalPages);
+
+            // Extract text from the specified pages
+            $chunkText = extractTextFromPDFPages($pdf, $startPage, $endPage);
+
+            // Set the data for the API request
+            $data = [
+                'model' => 'gpt-3.5-turbo-1106',
+                'messages' => [
+                    ['role' => 'system', 'content' => 'You are a helpful assistant.'],
+                    ['role' => 'user', 'content' => "Analyze the following text and provide a date of performed blood analysis and the name of the person , and could you tell me the blood analsysis (tests) Names and their values found (all of them in the text) ,and the general summary from the text and i need the response to be like this: Date: (the date)\n\nName Person:\n(name person)\n\nGeneral Summary:\n (the summary)\n\nAnalysis and Values:\n(the analysis) - (the values) \n\n" . $chunkText],
+                ],
+                'max_tokens' => 1000,  // You can adjust this based on the desired length of the completion
+            ];
+
+            // Initialize cURL session
+            $ch = curl_init($endpoint);
+
+            // Set cURL options
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+            // Execute cURL session and get the response
+            $response = curl_exec($ch);
+
+            // Check for cURL errors
+            if (curl_errno($ch)) {
+                echo 'Curl error: ' . curl_error($ch);
+                // Handle the error as needed
+            }
+
+            // Close cURL session
+            curl_close($ch);
+
+            // Decode the JSON response
+            $response_data = json_decode($response, true);
+
+            // Check if 'choices' key exists and is not null
+            if (isset($response_data['choices']) && !empty($response_data['choices'])) {
+                $choices = $response_data['choices'];
+                $generated_text = $choices[0]['message']['content'];
+                // Extracted content from the user message
+                $responses[] = $generated_text;
+            } else {
+                echo "No choices in the response.\n";
+            }
+        }
+
+        // Combine responses from chunks into a single string
+        $combined_response = implode('', $responses);
+
+        return $combined_response;
     } else {
-        echo "No choices in the response.\n";
+        // Set the data for the API request
+        $data = [
+            'model' => 'gpt-3.5-turbo-1106',  // Specify the chat model
+            'messages' => [
+                ['role' => 'system', 'content' => 'You are a helpful assistant.'],
+                ['role' => 'user', 'content' => "Analyze the following text and provide a report number and the sample description of the item , and could you tell me the analytical (tests) Names and their ratings (all of them in the text) ,and the analytical summary from the text and i need the response to be like this: Report Number: (the number)\n\nItem Description:\n(item description)\n\nAnalytical Summary:\n (the summary)\n\nNames and Ratings:\n(the name) - (the rating) \n\n " . $input_text],
+            ],
+            'max_tokens' => 1000,  // You can adjust this based on the desired length of the completion
+        ];
+
+        // Initialize cURL session
+        $ch = curl_init($endpoint);
+
+        // Set cURL options
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        // Execute cURL session and get the response
+        $response = curl_exec($ch);
+        print_r($response);
+        // Check for cURL errors
+        if (curl_errno($ch)) {
+            echo 'Curl error: ' . curl_error($ch);
+            // Handle the error as needed
+        }
+
+        // Close cURL session
+        curl_close($ch);
+
+        // Decode the JSON response
+        $response_data = json_decode($response, true);
+
+        // Check if 'choices' key exists and is not null
+        if (isset($response_data['choices']) && !empty($response_data['choices'])) {
+            $choices = $response_data['choices'];
+            $generated_text = $choices[0]['message']['content'];
+            // Extracted content from the user message
+            return $generated_text;
+        } else {
+            echo "No choices in the response.\n";
+        }
     }
+}
+
+// Function to extract text from specific pages in a PDF using PDF Parser
+function extractTextFromPDFPages($pdf, $startPage, $endPage)
+{
+    $text = '';
+
+    foreach ($pdf->getPages() as $pageNumber => $page) {
+        if ($pageNumber >= $startPage && $pageNumber <= $endPage) {
+            $text .= $page->getText();
+        }
+    }
+
+    return $text;
 }
